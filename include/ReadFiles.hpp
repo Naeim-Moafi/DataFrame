84c3583fbd50a855dc2127a35643298c68d38c43
Naeim <snmoafinejad@iimcb.gov.pl>
2024-10-29 11:26:28 +0100

    initial commit

---
+++ include/ReadFiles.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file ReadFiles.hpp
+ * @author Naeim Moafinejad (snmoafinejad@iimcb.gov.pl, s.naeim.moafi.n@gmail.com)
+ * @brief A class for reading files with different delimiters
+ * @version 0.1
+ * @date 2024-10-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
