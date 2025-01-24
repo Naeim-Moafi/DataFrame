7419963fc2248e2e880b143bef4a60c0bcb28043
Naeim <snmoafinejad@iimcb.gov.pl>
2025-01-24 16:46:03 +0100

    back to string for each cell, and addional function to parse from text not files

--- include/ReadFiles.hpp
+++ include/ReadFiles.hpp
@@ -15,6 +15,7 @@
 #include <string>
 #include <string_view>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 #include "fmt/core.h"
@@ -35,9 +36,10 @@ namespace std
         using VV_string = vector<V_string>;
         using V_any = vector<any>;
         using VV_any =vector<vector<any>>;
-        using Data = unordered_map<string, V_any>;
+        using Data = unordered_map<string, V_string>;
         using V_double = vector<double>;
         using V_int = vector<int>;
+        using V_pair_ints = std::vector<std::pair<int, int>>;
     }   
 }
 
@@ -100,7 +102,7 @@ namespace DF
             std::shorts::V_string get_headers() const;
 
 
-            std::shorts::V_any get_by_header(std::string const& hdr);
+            std::shorts::V_string get_by_header(std::string const& hdr);
 
             /**
              * @brief to copy current data into new data frame
@@ -140,7 +142,7 @@ namespace DF
              * @param v_cols_length 
              * @param is_first_col_header 
              */
-            void read_files(std::string_view path,std::shorts::V_int const& v_cols_length, bool is_first_col_header = true, std::shorts::V_string v_hdrs = {});
+            void read_text(std::string const& text,std::shorts::V_pair_ints const& v_cols_start_length, bool is_first_col_header = true, std::shorts::V_string v_hdrs = {});
 
             /**
              * @brief print n first rows off all columns
@@ -162,8 +164,11 @@ namespace DF
             std::shorts::V_string headers;
 
             std::shorts::V_string read_lines(std::string_view path);
-            void fill_data(std::shorts::V_string const& v_strs, char delim = ',', bool is_first_col_header = true, std::shorts::V_string v_hdrs = {});
-            void fill_data(std::shorts::V_string const& v_strs, std::shorts::V_int const& v_cols_length, bool is_first_col_header = true, std::shorts::V_string v_hdrs = {});
+            std::shorts::V_string read_lines(std::string const& text);
+            std::shorts::V_string parse_line(std::string const& line, char delim);
+            std::shorts::V_string parse_line(std::string const& line, std::shorts::V_pair_ints const& v_cols_start_ends);
+            void fill_data(std::shorts::V_string const& v_lines, char delim = ',', bool is_first_col_header = true, std::shorts::V_string v_hdrs = {});
+            void fill_data(std::shorts::V_string const& v_lines, std::shorts::V_pair_ints const& v_cols_start_length, bool is_first_col_header = true, std::shorts::V_string v_hdrs = {});
     };
     
 }
--- src/ReadFiles.cpp
+++ src/ReadFiles.cpp
@@ -43,28 +43,70 @@ std::shorts::V_string DF::DataFrame::read_lines(std::string_view path)
     return v_strs;
 }
 
+std::shorts::V_string DF::DataFrame::read_lines(std::string const& text)
+{
+    std::istringstream iss(text);
+
+    std::string line;
+    std::shorts::V_string v_strs;
+
+
+    while(std::getline(iss, line))
+    {
+        if(line.size() == 0) continue;
+        line.erase(std::remove(line.begin(), line.end(), '\r'),line.end());
+        v_strs.emplace_back(line);
+    }
+
+    return v_strs;
+}
+
+
+std::shorts::V_string DF::DataFrame::parse_line(std::string const& line,char delim)
+{
+    std::shorts::V_string v_strs;
+
+    std::istringstream iss(line);
+    std::string cell;
+
+    while(std::getline(iss, cell, delim))
+    {
+        cell.erase(std::remove(cell.begin(), cell.end(), '\"'), cell.end());
+        cell.erase(std::remove(cell.begin(), cell.end(), ' '), cell.end());
+        v_strs.emplace_back(cell);
+    }
+
+    return v_strs;
+}
+
+std::shorts::V_string DF::DataFrame::parse_line(std::string const& line,std::shorts::V_pair_ints const& v_cols_start_ends)
+{
+    std::shorts::V_string v_strs;
+
+    for(auto const& [start, end] : v_cols_start_ends)
+    {
+        auto cell = line.substr(start, end);
+        cell.erase(std::remove(cell.begin(), cell.end(), '\"'), cell.end());
+        cell.erase(std::remove(cell.begin(), cell.end(), ' '), cell.end());
+        v_strs.push_back(cell);
+    }
+
+    return v_strs;
+}
 
 void DF::DataFrame::fill_data(std::shorts::V_string const& lines, char delim, bool is_first_col_header, std::shorts::V_string v_hdrs)
 {
-    std::shorts::VV_any vv_anys;
+    std::shorts::VV_string vv_strs;
     
     for(auto const& line : lines)
     {
-        std::istringstream iss(line);
-        std::string cell;
-        std::shorts::V_any v_str_tmp;
-
-        while(std::getline(iss, cell, delim))
-        {
-            cell.erase(std::remove(cell.begin(), cell.end(), '\"'), cell.end());
-            v_str_tmp.emplace_back(cell);
-        }
-        vv_anys.emplace_back(v_str_tmp);
+        std::shorts::V_string v_str_tmp = parse_line(line, delim);
+        vv_strs.emplace_back(v_str_tmp);
     }
 
     // init n_rows and n_cols
-    n_rows = vv_anys.size();
-    n_cols = vv_anys[0].size();
+    n_rows = vv_strs.size();
+    n_cols = vv_strs[0].size();
 
     headers.resize(n_cols);
 
@@ -73,7 +115,7 @@ void DF::DataFrame::fill_data(std::shorts::V_string const& lines, char delim, bo
     {
         if(is_first_col_header)
         {
-            headers[i_col] = std::any_cast<std::string>(vv_anys[0][i_col]); 
+            headers[i_col] = vv_strs[0][i_col]; 
         }
         else if(v_hdrs.size() > 0)
         {
@@ -94,21 +136,87 @@ void DF::DataFrame::fill_data(std::shorts::V_string const& lines, char delim, bo
 
     for(unsigned long long i_col{0}; i_col < n_cols; ++i_col)
     {
-        std::shorts::V_any values;
+        std::shorts::V_string values;
         for(unsigned long long i_row{is_first_col_header}; i_row < n_rows; ++i_row)
         {
-            if(vv_anys[i_row].size() != n_cols)
+            if(vv_strs[i_row].size() != n_cols)
             {
                 throw std::runtime_error(fmt::format(fg(fmt::color::red), "Error: inconsistent number of columns, check row {}", i_row + 1));
             }
 
-            values.emplace_back(vv_anys[i_row][i_col]);
+            values.emplace_back(vv_strs[i_row][i_col]);
 
             // check for the missig values
             // missing values are empty string, NA, and NAN
-            if(std::any_cast<std::string>(vv_anys[i_row][i_col]) == ""   || 
-               std::any_cast<std::string>(vv_anys[i_row][i_col]) == "NA" || 
-               std::any_cast<std::string>(vv_anys[i_row][i_col]) == "NAN")
+            if(vv_strs[i_row][i_col] == ""   || 
+               vv_strs[i_row][i_col] == "NA" || 
+               vv_strs[i_row][i_col] == "NAN")
+            {
+                mising_values.insert({i_row, i_col});           
+            }
+        }
+
+        data[headers[i_col]] = values;
+    }
+}
+
+void DF::DataFrame::fill_data(std::shorts::V_string const& lines, std::shorts::V_pair_ints const& v_cols_start_length, bool is_first_col_header, std::shorts::V_string v_hdrs)
+{
+    std::shorts::VV_string vv_strs;
+    for(auto const& line : lines)
+    {
+        auto v_str_tmp = parse_line(line, v_cols_start_length);
+        vv_strs.emplace_back(v_str_tmp);
+    }
+
+    // init n_rows and n_cols
+    n_rows = vv_strs.size();
+    n_cols = vv_strs[0].size();
+
+    headers.resize(n_cols);
+
+    // initializing headers
+    for(unsigned long long i_col{0}; i_col < n_cols; ++i_col)
+    {
+        if(is_first_col_header)
+        {
+            headers[i_col] = vv_strs[0][i_col]; 
+        }
+        else if(v_hdrs.size() > 0)
+        {
+            if(v_hdrs.size() == n_cols)
+            {
+                headers[i_col] = v_hdrs[i_col];
+            }
+            else
+            {
+                throw std::runtime_error(fmt::format(fg(fmt::color::red), "Error: number of provided headers does not match with the number of columns in the data"));
+            }
+        }
+        else
+        {
+            headers[i_col] = std::to_string(i_col + 1);
+        }
+    }
+
+    for(unsigned long long i_col{0}; i_col < n_cols; ++i_col)
+    {
+        std::shorts::V_string values;
+        for(unsigned long long i_row{is_first_col_header}; i_row < n_rows; ++i_row)
+        {
+            if(vv_strs[i_row].size() != n_cols)
+            {
+                throw std::runtime_error(fmt::format(fg(fmt::color::red), "Error: inconsistent number of columns, check row {}", i_row + 1));
+            }
+
+            values.emplace_back(vv_strs[i_row][i_col]);
+
+            // check for the missig values
+            // missing values are empty string, NA, and NAN
+            if(vv_strs[i_row][i_col].empty() ||
+               vv_strs[i_row][i_col] == " "  || 
+               vv_strs[i_row][i_col] == "NA" || 
+               vv_strs[i_row][i_col] == "NAN")
             {
                 mising_values.insert({i_row, i_col});           
             }
@@ -120,15 +228,21 @@ void DF::DataFrame::fill_data(std::shorts::V_string const& lines, char delim, bo
 
 void DF::DataFrame::read_files(std::string_view path, char delim, bool is_first_col_header, std::shorts::V_string v_hdrs)
 {
-    auto v_strs = read_lines(path);
-    fill_data(v_strs, delim, is_first_col_header, v_hdrs);
+    auto lines = read_lines(path);
+    fill_data(lines, delim, is_first_col_header, v_hdrs);
+}
+
+void DF::DataFrame::read_text(std::string const& text,std::shorts::V_pair_ints const& v_cols_start_length, bool is_first_col_header, std::shorts::V_string v_hdrs)
+{
+    auto lines = read_lines(text);
+    fill_data(lines, v_cols_start_length, is_first_col_header, v_hdrs);
 }
 
 void DF::DataFrame::head(unsigned long long n)
 {
     if(n > n_rows)
     {
-        fmt::print(fg(fmt::color::yellow),"Warning: number of columns requested ({}) to be printed is bigger than number of available rows in data ({})\n",
+        fmt::print(fg(fmt::color::yellow),"Warning: number of row requested ({}) to be printed is bigger than number of available rows in data ({})\n",
         n, data[headers[0]].size());
         if(n_rows > 5) n = 5;
         else n = n_rows;
@@ -136,24 +250,35 @@ void DF::DataFrame::head(unsigned long long n)
 
     for(auto const& curr_hdr : headers)
     {
-        fmt::print(fmt::emphasis::bold | fg(fmt::color::green), "{:10} ", curr_hdr);
+        fmt::print(fmt::emphasis::bold | fg(fmt::color::green), "{:^15}|", curr_hdr); 
     }
 
-    std::cout << '\n';
+    std::string sub_separator = std::string(15,'-') + '+'; 
+    std::string separator = "";
+
+    for(size_t i{0}; i < n_cols; ++i )
+    {
+        separator += sub_separator; 
+    }
+    // fmt::println("\n{}",std::string(headers.size() * 16, '-'));
+    fmt::println("\n{}",std::string(separator));
+
+    // std::cout << '\n';
 
     for(unsigned long long i{0}; i < n; ++i)
     {
         for(auto const& curr_hdr : headers)
         {
-            if(std::any_cast<std::string>(data[curr_hdr][i]) == ""   || 
-               std::any_cast<std::string>(data[curr_hdr][i]) == "NA" || 
-               std::any_cast<std::string>(data[curr_hdr][i]) == "NAN")
+            if(data[curr_hdr][i] == ""   || 
+               data[curr_hdr][i] == "NA" || 
+               data[curr_hdr][i] == "NAN")
             {
-                fmt::print(bg(fmt::color::red),"{:10} ", std::any_cast<std::string>(data[curr_hdr][i]) /*data[curr_hdr][i]*/);
+                fmt::print(bg(fmt::color::red),"{:^15}", data[curr_hdr][i] /*data[curr_hdr][i]*/);
+                std::cout << "|";
             }
             else
             {
-                fmt::print("{:10} ", std::any_cast<std::string>(data[curr_hdr][i]) /*data[curr_hdr][i]*/);
+                fmt::print("{:^15}|", data[curr_hdr][i] /*data[curr_hdr][i]*/);
             }
             
         }
@@ -167,7 +292,7 @@ std::shorts::V_string DF::DataFrame::get_headers() const
     return headers;
 }
 
-std::shorts::V_any DF::DataFrame::get_by_header(std::string const& hdr)
+std::shorts::V_string DF::DataFrame::get_by_header(std::string const& hdr)
 {
     return data[hdr];
 }
@@ -199,9 +324,15 @@ void DF::DataFrame::swap_cols_pos(std::string first_hdr, std::string second_hdr)
 }
 
 
-
 int main()
 {
+    std::string software_name = R"( ____        _        _____                         
+|  _ \  __ _| |_ __ _|  ___| __ __ _ _ __ ___   ___ 
+| | | |/ _` | __/ _` | |_ | '__/ _` | '_ ` _ \ / _ \
+| |_| | (_| | || (_| |  _|| | | (_| | | | | | |  __/
+|____/ \__,_|\__\__,_|_|  |_|  \__,_|_| |_| |_|\___|)";
+
+        std::cout << software_name << std::endl << std::endl;
     DF::DataFrame df;
 
 
@@ -226,9 +357,25 @@ int main()
     std::cout << '\n';
 
     df.head(600);
+    std::cout << '\n';
+
+    DF::DataFrame df3;
+    std::vector<std::string> v_hdrs {"group_PDB", "id", "label_atom_id", "label_comp_id", "label_asym_id", 
+                                     "label_seq_id", "Cartn_x", "Cartn_y", "Cartn_z", "occupancy", 
+                                     "B_iso_or_equiv", "type_symbol", "charge"};
+
+    std::shorts::V_pair_ints fields_intervals = {{0,6}, {6,5}, {12,4}, {17,3}, {20,2}, {22,4}, {30,8}, {38,8}, {46,8}, {54,6}, {60,6}, {76,2}, {78,2}};
+
+    // std::string text{"HETATM    1  PG  GTP A   1      24.181  32.064  27.670  0.10 24.73           P  \nHETATM    2  O1G GTP A   1      24.342  33.433  27.064  0.10 37.56           O  \nHETATM    3  O2G GTP A   1      24.519  32.013  29.136  0.10 32.46           O  \n"};
+    std::string text = R"(HETATM    1  PG  GTP A   1      24.181  32.064  27.670  0.10 24.73           P  
+HETATM    2  O1G GTP A   1      24.342  33.433  27.064  0.10 37.56           O  
+HETATM    3  O2G GTP A   1      24.519  32.013  29.136  0.10 32.46           O  
+HETATM    4  O3G GTP A   1      25.048  31.062  26.907  1.00 47.91           O  
+HETATM    5  O3B GTP A   1      22.644  31.665  27.526  1.00 33.11           O )";
+    
+    df3.read_text(text, fields_intervals, false, v_hdrs);
+    df3.head();
 
 
     return EXIT_SUCCESS;
 }
-
-
